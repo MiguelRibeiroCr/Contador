4dfd540ccaeef716f92218894fb04963e35c105d
MiguelRibeiroCr <miguelribe715@gmail.com>
2023-05-31 21:39:58 -0300

    Contador

--- App.tsx
+++ App.tsx
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import {StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import * as Font from 'expo-font'
 
 export default function App() {
+
+  const [fonteCarregada, setFonteCarregada] = useState(false);
+  const [Somar, setSoma] = useState(0);
+  const Contador = () => {
+    setSoma(Somar + 1);
+  };
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      geologic_regular: require('./assets/Geologica/static/Geologica_Auto-Regular.ttf'),
+      geologic_bold: require('./assets/Geologica/static/Geologica-Bold.ttf'),
+      geologic_light: require('./assets/Geologica/static/Geologica-Light.ttf')
+    });
+  }
+
+  useEffect(() => {
+    loadFonts()
+      .then(() => {
+        setFonteCarregada(true);
+      })
+      .catch((error) => {
+        console.log('Ocorreu um erro', error);
+      })
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      {fonteCarregada ?
+        <Text style={{ fontFamily: 'geologic_regular', fontSize: 30 }}>Bem vindo!</Text>
+        :
+        <Text>Bem vindo!</Text>
+      }
+      <View>
+        <Text style={{ fontSize: 50 }}>Contador: {Somar} </Text>
+      </View>
+
+        <View style={styles.Center}>
+          <TouchableHighlight
+          style={styles.button}
+          onPress={Contador}>
+            <Text style={{ fontSize: 15 }}>Clique para somar</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+      
+    
   );
 }
 
@@ -17,4 +59,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button:{
+    justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 15,
+        backgroundColor: 'pink',
+        width: 150,
+        height: 150,
+        borderRadius: 100,
+        position: 'absolute',
+        marginTop:120
+  },
+
+  Center:{
+    alignItems: 'center',
+   
+}
 });
---
+++ test.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default  () => {
+   
+
+  const [Somar, setSoma] = useState(0);
+
+  const Contador = () => {
+    setSoma(Somar + 1);
+  };
+
+  return (
+    <View>
+      <Text style={{ fontSize: 30, textAlign: 'center', marginVertical: 20, marginTop:100  }}>
+        Contador: {Somar}
+      </Text>
+  
+  <View  style={styles.button}>
+      <Button
+      title="Clique para somar" onPress={Contador} />
+      </View>
+    </View>
+  );
+};
+   
+
+ 
+  
+
+ 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  },
+  button:{
+    marginVertical:350,
+    marginHorizontal:140,
+    width:150,
+    borderRadius:100,
+
+   
+
+  }
+});
